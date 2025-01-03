8612511e94fd824d71ccd7fc09b6cc82443399a9
Ryan Willmore <ryanwillmore@gmail.com>
2025-01-03 12:19:03 -0700

    fix images and fonts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/willmore-lumber",
+  assetPrefix: "/willmore-lumber",
 };
 
 export default nextConfig;
