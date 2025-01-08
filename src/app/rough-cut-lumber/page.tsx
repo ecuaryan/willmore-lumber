1a616fe08eedd74031f1e73db54f8aba5236ee08
Ryan Willmore <ryanwillmore@gmail.com>
2025-01-08 13:56:49 -0700

    rough-cut lumber page

--- src/app/rough-cut-lumber/page.tsx
+++ src/app/rough-cut-lumber/page.tsx
@@ -1,16 +1,60 @@
-import { Typography } from "@mui/material";
+/* eslint-disable @next/next/no-img-element */
+import { List, ListItem, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 export default function RoughCutLumber() {
   return (
-    <>
-      <Typography variant="h5">Rough-Cut Lumber</Typography>
-      {/*
-       - dimensional lumber
-       - maybe a link back to the post and beams page from here
-       - fencing
-       - corrals 
-       - farm and ranch lumber 
-       */}
-    </>
+    <Grid container spacing={4}>
+      <Grid size={12}>
+        <Stack spacing={3} maxWidth="700px">
+          <Typography variant="h3">Rough-Cut Lumber</Typography>
+          <Typography variant="body1">
+            We carry a wide variety of lengths and sizes in premium rough-cut
+            lumber, ready to meet your project needs.
+          </Typography>
+          <Typography variant="body1">
+            Here are some common uses for rough-cut lumber:
+            <List>
+              <ListItem>farm and ranch</ListItem>
+              <ListItem>corral fencing</ListItem>
+              <ListItem>rustic wood projects</ListItem>
+              <ListItem>garden/flower boxes</ListItem>
+            </List>
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/rough-cut-1.webp"
+          alt="Rough-cut lumber"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/rough-cut-2.webp"
+          alt="Rough-cut lumber"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/rough-cut-3.webp"
+          alt="Rough-cut lumber"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/fencing.jpg"
+          alt="fencing"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+    </Grid>
   );
 }
