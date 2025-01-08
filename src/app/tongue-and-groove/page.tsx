790d601f4cfd239d75ae68c670847bc5734d6224
Ryan Willmore <ryanwillmore@gmail.com>
2025-01-08 15:32:35 -0700

    tongue and groove

--- src/app/tongue-and-groove/page.tsx
+++ src/app/tongue-and-groove/page.tsx
@@ -1,16 +1,34 @@
-import { Typography } from "@mui/material";
+/* eslint-disable @next/next/no-img-element */
+import { List, ListItem, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 export default function TongueAndGroove() {
   return (
-    <>
-      <Typography variant="h5">
-        Tongue and Groove - This page is coming soon.
-      </Typography>
-      {/*
-       - knotty pine
-       - Douglas fir
-       - smooth or resawn
-       */}
-    </>
+    <Grid container spacing={4}>
+      <Grid size={12}>
+        <Stack spacing={3} maxWidth="700px">
+          <Typography variant="h3">Tongue and Groove</Typography>
+          <Typography variant="body1">
+            Discover the natural beauty of knotty pine and Douglas fir
+            tongue-and-groove boards, perfect for a wide range of applications.
+          </Typography>
+          <Typography variant="body1">
+            <List>
+              <ListItem>knotty pine</ListItem>
+              <ListItem>Douglas fir</ListItem>
+              <ListItem>rough or resawn</ListItem>
+            </List>
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/tongue-and-groove.webp"
+          alt="tongue and groove"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+    </Grid>
   );
 }
