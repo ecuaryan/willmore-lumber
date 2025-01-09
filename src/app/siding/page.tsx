27cd7800aed143b1cfdaa3e6e629d79e28258273
Ryan Willmore <ryanwillmore@gmail.com>
2025-01-08 17:26:27 -0700

    siding page

--- src/app/siding/page.tsx
+++ src/app/siding/page.tsx
@@ -1,16 +1,85 @@
-import { Typography } from "@mui/material";
+/* eslint-disable @next/next/no-img-element */
+import { List, ListItem, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 export default function Siding() {
   return (
-    <>
-      <Typography variant="h5">Siding - This page is coming soon.</Typography>
-      {/*
-       - channel rustic shiplap siding - various patterns available
-       - log cabin siding
-       - hand hewn siding
-       - board and batten siding
-       - tongue and groove siding
-       */}
-    </>
+    <Grid container spacing={4}>
+      <Grid size={12}>
+        <Stack spacing={3} maxWidth="700px">
+          <Typography variant="h3">Siding</Typography>
+          <Typography variant="body1">
+            Elevate your home&apos;s curb appeal with our high-quality wood
+            siding, crafted for both durability and aesthetic appeal. Perfect
+            for a lasting first impression.
+          </Typography>
+          <Typography variant="body1">
+            <List>
+              <ListItem>
+                channel rustic shiplap siding - various patterns available
+              </ListItem>
+              <ListItem>log cabin siding</ListItem>
+              <ListItem>hand hewn siding</ListItem>
+              <ListItem>board and batten siding</ListItem>
+              <ListItem>tongue and groove siding</ListItem>
+            </List>
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/siding-6.webp"
+          alt="siding"
+          title="siding"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/siding.webp"
+          alt="siding"
+          title="siding"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/siding-5.webp"
+          alt="siding"
+          title="siding"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/siding-1.webp"
+          alt="siding"
+          title="siding"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/siding-4.webp"
+          alt="siding"
+          title="siding"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <img
+          src="/siding-2.webp"
+          alt="siding"
+          title="siding"
+          loading="lazy"
+          style={{ width: "100%" }}
+        />
+      </Grid>
+    </Grid>
   );
 }
