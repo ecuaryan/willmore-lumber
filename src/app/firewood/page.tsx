6c6322f4ed33ec1d7207cb826726e2060b01876b
Ryan Willmore <ryanwillmore@gmail.com>
2025-01-09 10:18:23 -0700

    firewood page

--- src/app/firewood/page.tsx
+++ src/app/firewood/page.tsx
@@ -1,13 +1,39 @@
-import { Typography } from "@mui/material";
+import { List, ListItem, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import Image from "next/image";
 
 export default function Firewood() {
   return (
-    <>
-      <Typography variant="h5">Firewood - This page is coming soon.</Typography>
-      {/*
-       - Firewood is available.
-       - anywhere from a few pieces to many cords
-       */}
-    </>
+    <Grid container spacing={4}>
+      <Grid size={12}>
+        <Stack spacing={3} maxWidth="700px">
+          <Typography variant="h3">Firewood</Typography>
+          <Typography variant="body1">
+            Stock up on premium firewood, cut and seasoned for efficient
+            burning. Perfect for fireplaces, wood stoves, and outdoor fire pits.
+          </Typography>
+          <Typography variant="body1">
+            <List>
+              <ListItem>
+                anywhere from a few pieces to many cords available
+              </ListItem>
+            </List>
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <Image
+          src="/firewood.jpg"
+          alt="firewood"
+          title="firewood pile"
+          loading="lazy"
+          style={{ width: "100%", height: "auto" }}
+          width={1333}
+          height={750}
+        />
+      </Grid>
+    </Grid>
   );
 }
+
+// TODO: Add another close up picture of the firewood
