c781c80ba95b9cf1efcac65c3ca76b31d1a31456
Ryan Willmore <ryanwillmore@gmail.com>
2025-01-09 10:54:58 -0700

    custom cut lumber page

--- src/app/custom-cut-lumber/page.tsx
+++ src/app/custom-cut-lumber/page.tsx
@@ -1,17 +1,39 @@
-import { Typography } from "@mui/material";
+import { List, ListItem, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import Image from "next/image";
 
 export default function CustomCutLumber() {
   return (
-    <>
-      <Typography variant="h5">
-        Custom-Cut Lumber - This page is coming soon.
-      </Typography>
-      {/*
-       - live edge slabs for counter tops and mantels
-       - angled ridge beams
-       - wedge shaped lumber
-       - Custom cut lumber for unique projects
-       */}
-    </>
+    <Grid container spacing={4}>
+      <Grid size={12}>
+        <Stack spacing={3} maxWidth="700px">
+          <Typography variant="h3">Custom Cut Lumber</Typography>
+          <Typography variant="body1">
+            We custom-cut lumber to perfectly match your specifications.
+          </Typography>
+          <Typography variant="body1">
+            <List>
+              <ListItem>live edge slabs for counter tops and mantels</ListItem>
+              <ListItem>angled ridge beams</ListItem>
+              <ListItem>wedge shaped lumber</ListItem>
+            </List>
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid size={{ xs: 12, md: 6 }}>
+        <Image
+          src="/custom-beams.webp"
+          alt="custom cut lumber"
+          title="wedge shaped beams"
+          loading="lazy"
+          style={{ width: "100%", height: "auto" }}
+          width={1000}
+          height={750}
+        />
+      </Grid>
+    </Grid>
   );
 }
+
+// TODO: add more description here?
+// TODO: more images, find that one with the mantel and the one with the crazy angles
--- src/app/wood-stain/page.tsx
+++ src/app/wood-stain/page.tsx
@@ -58,4 +58,4 @@ export default function WoodStain() {
   );
 }
 
-// TODO: could use another iamge
+// TODO: could use another image
